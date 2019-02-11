143144e64fe4eb8e8eea3df8a9735fa240ae8e63
vila-selena <admin@vila-selena>
2019-02-11 22:23:23 +0100

    apartments, besedilo in nekaj popravkov

--- js/main.js
+++ js/main.js
@@ -1,25 +1,5 @@
 $(document).ready(function(){
 
-/*
-    // handles the carousel thumbnails
-    $('[id^=carousel-selector-]').click( function(){
-        var id_selector = $(this).attr("id");
-        var id = id_selector.substr(id_selector.lastIndexOf('-') + 1);
-        id = parseInt(id);
-        $('#rsmm-gallery-carousel').carousel(id);
-        $('[id^=carousel-selector-]').removeClass('selected');
-        $(this).addClass('selected');
-    });
-
-    // when the carousel slides, auto update
-    $('#rsmm-gallery-carousel').on('slid.bs.carousel', function (e) {
-        var id = $('.item.active').data('slide-number');
-        id = parseInt(id);
-        $('[id^=carousel-selector-]').removeClass('selected');
-        $('[id=carousel-selector-'+id+']').addClass('selected');
-    });
-*/
-
     // panorama on 1st page
     if ( $('#panorama-banner').length ) {
         $('#panorama-banner').spritespin({
@@ -41,7 +21,30 @@ $(document).ready(function(){
         });
     };
 
-    // image zoom for gallery
+    // image zoom for about
+    if ( $('#about-popup-container').length ) {
+        $('#about-popup-container').magnificPopup({
+                delegate: 'a', // child items selector, by clicking on it popup will open
+                type: 'image',
+                gallery: {
+                        enabled: true,
+                        tCounter: ''
+                },
+                closeOnContentClick: true,
+                closeBtnInside: false,
+                fixedContentPos: true,
+                mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+                image: {
+                        verticalFit: true
+                },
+                zoom: {
+                        enabled: true,
+                        duration: 300 // don't foget to change the duration also in CSS
+                }
+        });
+    }
+    
+    // image zoom for about
     if ( $('#gallery-popup-container').length ) {
         $('#gallery-popup-container').magnificPopup({
                 delegate: 'a', // child items selector, by clicking on it popup will open
@@ -62,8 +65,8 @@ $(document).ready(function(){
                         duration: 300 // don't foget to change the duration also in CSS
                 }
         });
-    };
-
+    }
+    
     // socials icons in footer
     $("#social-share").jsSocials({
         showLabel: false,
@@ -72,26 +75,23 @@ $(document).ready(function(){
     });
 });
 
+
 /*
-$(function(){
-    if ( $('#panorama-banner').length ) {
-        $('#panorama-banner').spritespin({
-            source: '/images/banner/panorama.jpg',
-            // this sets the width of the display. The panorama image must be larger
-            responsive: true,
-            width: 600,
-            height: 300,
-            // select the modules
-            plugins: [
-                // change frame on mouse drag
-                'drag',
-                // enable the easing module. this will ease out the animation
-                // after mouse release, instead of a hard stop
-                'ease',
-                // the panorama display module
-                'panorama'
-            ]
-        });
-    };
-});
+    // handles the carousel thumbnails
+    $('[id^=carousel-selector-]').click( function(){
+        var id_selector = $(this).attr("id");
+        var id = id_selector.substr(id_selector.lastIndexOf('-') + 1);
+        id = parseInt(id);
+        $('#rsmm-gallery-carousel').carousel(id);
+        $('[id^=carousel-selector-]').removeClass('selected');
+        $(this).addClass('selected');
+    });
+
+    // when the carousel slides, auto update
+    $('#rsmm-gallery-carousel').on('slid.bs.carousel', function (e) {
+        var id = $('.item.active').data('slide-number');
+        id = parseInt(id);
+        $('[id^=carousel-selector-]').removeClass('selected');
+        $('[id=carousel-selector-'+id+']').addClass('selected');
+    });
 */
