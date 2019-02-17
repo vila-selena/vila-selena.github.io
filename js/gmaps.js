9b2e2da0d71b69fff365d4fd7455f797a4d2b7e8
vila-selena <admin@vila-selena>
2019-02-17 22:11:03 +0100

    popravki lokacija, rezervacija in oblikovanje

--- js/gmaps.js
+++ js/gmaps.js
@@ -1,3 +1,37 @@
+function MapTypeControl(controlDiv, map) {
+    // control border
+    var controlUI = document.createElement('div');
+    controlUI.style.border = '3px solid #fff';
+    controlUI.style.cursor = 'pointer';
+    controlUI.style.width = '80px';
+    controlUI.style.marginLeft = '20px';
+    controlUI.title = 'Satellite';
+    controlDiv.appendChild(controlUI);
+
+    // control interior
+    var controlPic = document.createElement('img');
+    controlPic.src = '/images/map-satelit.jpg';
+    controlPic.style.width = '100%';
+    controlPic.style.margin = '0px';
+    controlUI.appendChild(controlPic);
+
+    // event
+    controlUI.addEventListener('click', function() {
+        var mt = map.getMapTypeId();
+        if (mt == 'roadmap') {
+            controlUI.title = 'Street map';
+            controlPic.src = '/images/map-street.jpg';  
+            map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
+        }
+        else if (mt == 'satellite') {
+            controlUI.title = 'Satellite map';
+            controlPic.src = '/images/map-satelit.jpg';  
+            map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
+        };
+    });
+}
+
+
 function setMarker(text, width, position, map) {
     const h = 40;
     const r = 12;
@@ -28,6 +62,7 @@ function setMarker(text, width, position, map) {
         map: map,
         label: myLabel,
         icon: myIcon,
+        clickable: true,        
     });
     
     return myMarker;
@@ -36,27 +71,36 @@ function setMarker(text, width, position, map) {
 
 // show google map for location
 function initMap() {
-
-    var mapCenter = {lat: 46.055, lng: 14.507};
+    
+    var vilaSelena = {lat: 46.056812, lng: 14.503735};
+//    var mapCenter = {lat: 46.055, lng: 14.507};
+    var mapCenter = vilaSelena;
+    
     var map = new google.maps.Map(document.getElementById('map'), {
         center:  mapCenter,
-        zoom: 16,
+        zoom: 15,
         mapTypeControl: false,
         fullscreenControl: false,
         zoomControl: true,
         scaleControl: true,
         streetViewControl: false,
-        styles: [{
+        styles: [
+            {
                 featureType: 'poi',
-                stylers: [{visibility: 'off'}]
-            }],
+                stylers: [{visibility: 'off'}],
+            },
+            {
+                featureType: 'poi.park',
+                stylers: [{visibility: 'on'}],
+            }
+        ],
     });
+
+    var mSelena = setMarker('Vila Selena', 50, vilaSelena, map);
     
-    var vilaSelena = {lat: 46.056812, lng: 14.503735};
-    var selenaMarker = setMarker('Vila Selena', 50, vilaSelena, map);
-    var circle10m = new google.maps.Circle({
+    var c10m = new google.maps.Circle({
         strokeColor: '#783226',  
-        strokeOpacity: 0.7,
+        strokeOpacity: 1.0,
         strokeWeight: 1,
         fillColor: '#783226',
         fillOpacity: 0.0,
@@ -65,15 +109,13 @@ function initMap() {
         radius: 666,
         clickable: false,
     });
-    
-    var vilaSelena10m = {lat: 46.0507, lng: 14.5039};
-    var marker10m = new google.maps.Marker({
-        position: vilaSelena10m,
+    var m10m = new google.maps.Marker({
+        position: {lat: 46.0506, lng: 14.5036},
         map: map,
         label: {
             color: '#783226',
             fontFamily: 'Palanquin, sans-serif',
-            fontSize: '16px',
+            fontSize: '18px',
             fontWeight: 'bold',
             text: '10 min hoje',
         },
@@ -86,24 +128,116 @@ function initMap() {
         clickable: false,
     });
 
-    setMarker('Železniška postaja', 80, {lat: 46.058555, lng: 14.516291}, map);
-    setMarker('Park Tivoli', 50, {lat: 46.056134, lng: 14.496694}, map);
-    setMarker('Prešernov trg', 60, {lat: 46.051242, lng: 14.506014}, map);
-    setMarker('Zmajski most', 60, {lat: 46.052098, lng: 14.510292}, map);
-    setMarker('Ljubljanski grad', 70, {lat: 46.048919, lng: 14.508608}, map);
+    var mBusRail = setMarker('Avtobusna in železniška postaja', 130, {lat: 46.058525, lng: 14.511328}, map); 
+    var mTivoli = setMarker('Park Tivoli', 50, {lat: 46.056134, lng: 14.496694}, map);
+    var mTromost = setMarker('Tromostovje', 60, {lat: 46.051016, lng: 14.506297}, map);
+    var mZmaj = setMarker('Zmajski most', 60, {lat: 46.052098, lng: 14.510292}, map);
+    var mGrad = setMarker('Ljubljanski grad', 70, {lat: 46.048919, lng: 14.508608}, map);
 
     // to airpport 
-    var line = new google.maps.Polyline({
-        path: [{lat: 46.057499, lng: 14.500498}, {lat: 46.059435, lng: 14.498535}],
+    var mAirpLine = new google.maps.Polyline({
+        path: [{lat: 46.058994, lng: 14.498985}, {lat: 46.061689, lng: 14.496013}], 
         strokeColor: '#783226',
+        strokeOpacity: 1.0,
+        strokeWeight: 4,
         icons: [{
           icon: {path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW},
           offset: '100%'
         }],
         map: map,
     });
-    setMarker('Letališče Jožeta Pučnika (22km)', 130, {lat: 46.058103, lng: 14.499892}, map);
+    var mAirp22 = setMarker('Letališče Jožeta Pučnika (22km)', 130, {lat: 46.058994, lng: 14.498985}, map); 
+    var mAirport = setMarker('Letališče Jožeta Pučnika', 110, {lat: 46.233116, lng: 14.453966}, map);
 
+    // click actions
+    var mZoom = 15; // zoom to hide/show markers
+    map.addListener('zoom_changed', function() {
+        var z = map.getZoom();
+        if (z < mZoom) {
+            mBusRail.setVisible(false);
+            mTromost.setVisible(false);
+            mTivoli.setVisible(false);
+            mZmaj.setVisible(false);
+            mGrad.setVisible(false);
+            mAirpLine.setVisible(false);
+            mAirp22.setVisible(false);
+        }
+        else {
+            mBusRail.setVisible(true);
+            mTromost.setVisible(true);
+            mTivoli.setVisible(true);
+            mZmaj.setVisible(true);
+            mGrad.setVisible(true);
+            mAirpLine.setVisible(true);
+            mAirp22.setVisible(true);
+        }
+        
+        if (z < mZoom-1) {
+            c10m.setVisible(false);
+            m10m.setVisible(false);
+        }
+        else {
+            c10m.setVisible(true);
+            m10m.setVisible(true);
+        }
+    });
+    
+    mSelena.addListener('click', function() {
+        var z = map.getZoom();
+        if (z < mZoom-1) {
+            map.setZoom(15);
+            map.setCenter(mapCenter);
+        }
+        else
+        {
+            map.setZoom(z+1);
+            map.setCenter(vilaSelena);
+        }
+    });
+    
+    mAirp22.addListener('click', function() {
+        map.setZoom(11);
+        map.setCenter({lat: 46.142, lng: 14.503});
+    });
+
+    // open new info window for markers
+    mAirport.addListener('click', function() {
+        var cLink = 'https://www.fraport-slovenija.si/sl/Main';
+        window.open(cLink, "_blank");
+    });
+
+    mBusRail.addListener('click', function() {
+        var cLink = 'https://www.ap-ljubljana.si/';
+        window.open(cLink, "_blank");
+    });
+ 
+    mTivoli.addListener('click', function() {
+        var cLink = 'https://www.visitljubljana.com/sl/obiskovalci/aktivnosti/zabava/park-tivoli/';
+        window.open(cLink, "_blank");
+    });
+
+    mTromost.addListener('click', function() {
+        var cLink = 'https://www.visitljubljana.com/sl/obiskovalci/aktivnosti/znamenitosti/tromostovje/';
+        window.open(cLink, "_blank");
+    });
+
+    mZmaj.addListener('click', function() {
+        var cLink = 'https://www.visitljubljana.com/sl/obiskovalci/aktivnosti/znamenitosti/zmajski-most/';
+        window.open(cLink, "_blank");
+    });
+
+    mGrad.addListener('click', function() {
+        var cLink = 'https://www.ljubljanskigrad.si/sl/ljubljanski-grad/';
+        window.open(cLink, "_blank");
+    });
+
+
+    // map type control
+    var mtControlDiv = document.createElement('div');
+    var mtControl = new MapTypeControl(mtControlDiv, map);
+    mtControlDiv.index = 1;
+    map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(mtControlDiv);
+    
 } // initMap
 
 google.maps.event.addDomListener(window, 'load', initMap);
--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@ $(document).ready(function(){
         });
     }
     
-    // image zoom for about
+    // image zoom for gallery
     if ( $('#gallery-popup-container').length ) {
         $('#gallery-popup-container').magnificPopup({
                 delegate: 'a', // child items selector, by clicking on it popup will open
@@ -53,6 +53,25 @@ $(document).ready(function(){
                         enabled: true,
                         tCounter: ''
                 },
+                closeOnContentClick: true,
+                closeBtnInside: false,
+                fixedContentPos: true,
+                mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+                image: {
+                        verticalFit: true
+                },
+                zoom: {
+                        enabled: true,
+                        duration: 300 // don't foget to change the duration also in CSS
+                }
+        });
+    }
+    
+    // image zoom for location
+    if ( $('#location-popup-container').length ) {
+        $('#location-popup-container').magnificPopup({
+                delegate: 'a', // child items selector, by clicking on it popup will open
+                type: 'image',
                 closeOnContentClick: true,
                 closeBtnInside: false,
                 fixedContentPos: true,
